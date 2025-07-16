89408e007e095e2334206504256ae3e13b67e7a8
Aya Alika <149220082+Ayaalika@users.noreply.github.com>
2025-07-16 18:45:46 +0300

    Create Cell.java

---
+++ src/Cell.java
@@ -0,0 +1,10 @@
+class Cell {
+    int height, i, j;
+    boolean isRiver = false;
+    boolean isDesert = false;
+    Cell(int h, int i, int j) {
+        this.height = h;
+        this.i = i;
+        this.j = j;
+    }
+}
